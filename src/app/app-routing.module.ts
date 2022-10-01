729fb77a111d275144398fe947c5b41f73ad4964
erikjc29 <erikalejc@gmail.com>
2022-10-01 20:03:31 -0300

    Actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { LoginComponent } from './componente/login/login.component';
+import { RegisterComponent } from './componente/register/register.component';
 const routes: Routes = [
+ 
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path : 'login' , component : LoginComponent
+  } ,
+  {
+    path : 'register' , component : RegisterComponent
+  },
+   {
+    path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
   }
 ];
 @NgModule({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  data: any
+  constructor() {
+    
+async function getUser() {
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", "fefa0178d76ef3625775679a862e9154");
+myHeaders.append('Access-Control-Allow-Origin', '*');
+myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
 
-  constructor() {}
+    let response = await fetch("https://v3.football.api-sports.io/status", {
+      method: "GET", 
+      "headers": {
+        "x-rapidapi-key": "fefa0178d76ef3625775679a862e9154",
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+        'Access-Control-Allow-Origin':'*'
+    },redirect:"follow"});
+    let userData = await response.json();
+    console.log(userData)
+    this.data= userData;
+
+  // let response=await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+  // let datapoke=await response.json()
+  // return datapoke.name
+
+}
+
+  getUser().then(value=>{
+    this.data=value
+  })
+  }
+
+
 
 }
